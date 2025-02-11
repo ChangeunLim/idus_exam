d3c6c20e3cd80d79023aa45e88640d5a9ae6a5c4
LimChangeun <cksrms1030@naver.com>
2025-02-11 12:24:06 +0900

    [feat] patment add

--- src/main/java/com/example/idus_exam/Payment/PaymentController.java
+++ src/main/java/com/example/idus_exam/Payment/PaymentController.java
@@ -1,11 +1,16 @@
 package com.example.idus_exam.Payment;
 
 import com.example.idus_exam.Member.model.Member;
+import com.example.idus_exam.Payment.model.PaymentDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,5 +21,11 @@ public class PaymentController {
     @PostMapping("/register")
     public void register(@AuthenticationPrincipal Member member) {
         patmentService.register(dto, member);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<PaymentDto.PaymentResponse>> list() {
+        List<PaymentDto.PaymentResponse> res = patmentService.list();
+        return ResponseEntity.ok(res);
     }
 }
